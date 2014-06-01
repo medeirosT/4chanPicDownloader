27d34820873affa3cc50c8782505e008288df11c
Darkchild Heartly <madcat@Cromo>
2014-06-01 02:11:30 -0400

    Extra spacing, to follow Wordpress coding standards closer

--- downloader.php
+++ downloader.php
@@ -16,25 +16,25 @@ if ( $html = file_get_contents( $url ) ){
 
 	echo "Exploding data...\n";
 
-	preg_match_all('/(")(\\/)(\\/)(i\\.4cdn\\.org).*?(")/is', $html, $results);	// Just regex the files we're looking for.
+	preg_match_all( '/(")(\\/)(\\/)(i\\.4cdn\\.org).*?(")/is' , $html, $results );	// Just regex the files we're looking for.
 
-	$results = array_values(array_unique($results[0]));		// Clean results from REGEX
+	$results = array_values( array_unique( $results[0] ) );		// Clean results from REGEX
 
-	$totalResults = count($results);				// Get number of clean results
+	$totalResults = count( $results );				// Get number of clean results
 
-	foreach ($results as $currentResult => $result){
+	foreach ( $results as $currentResult => $result ){
 	
 		$currentResult++;					// So it will show the right number in output, not index;
-		$result = 'http:' . trim($result, '"');			// Clean out quotes from URL and add HTTP to it
+		$result = 'http:' . trim( $result, '"' );		// Clean out quotes from URL and add HTTP to it
 
-		$filename = basename($result);				// Get filename from URL.
+		$filename = basename( $result );				// Get filename from URL.
 
 		echo "[$currentResult/$totalResults] $filename ";	// Simple echo to show user current position within thread
 
 		try{	
-			$fileData = file_get_contents($result);					// Attempt to grab file from site's CDN
+			$fileData = file_get_contents( $result );				// Attempt to grab file from site's CDN
 		
-			if( $writeResult = file_put_contents($filename, $fileData) ){		// Attempt to write result to file
+			if( $writeResult = file_put_contents( $filename, $fileData ) ){		// Attempt to write result to file
 			
 				echo ".. OK!\n";
 
