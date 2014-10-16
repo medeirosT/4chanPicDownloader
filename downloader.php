65bb8099e9b7434f7229bb694a66d3d5c94aae48
Tiago Medeiros <madcat@cromio>
2014-10-16 02:31:00 -0400

    Updated the code, added retries capability (Buggy/Untested/dirty)

--- downloader.php
+++ downloader.php
@@ -27,65 +27,111 @@
 */
 
 // Input URL HERE :
-$url = "<INSERT FULL 4CHAN URL HERE>";
+$url = "";
 
+// Number of times to attempt connection / file download
+$retries = 3;
 
 // DO NOT EDIT ANYTHING BELOW THIS LINE!
 // --------------------------------------
 
 set_time_limit(0);							// If you have a slow internet this is a life saver!
+error_reporting(0);							// so only my errors show :)
 
-echo "Grabbing data...\n";
+$pageTryNumber = 1;
 
-if ( $html = file_get_contents( $url ) ){
 
-	echo "Exploding data...\n";
+// TODO : Clean up the retry code. It's dirty as heck
+// TODO : Redo output (Copying/pasting messages is dirty. function it!)
+// TODO : function the 404 check on the thread in its own loop! 
+// TODO : Show user more stats! (Low Priority)
+// TODO : Check if file_get_contents has a timeout that can be set (Maybe make a mode for people with bad internet connections)
+// TODO : General code clean up. Been a while since I touched this and I hate it.
 
-	preg_match_all( '/(")(\\/)(\\/)(i\\.4cdn\\.org).*?(")/is' , $html, $results );	// Just regex the files we're looking for.
 
-	$results = array_values( array_unique( $results[0] ) );		// Clean results from REGEX
+if ($retries < $pageTryNumber){ die("FATAL : At least make the # of retries bigger than 0! See config lines in beginning of script!"); }
 
-	$totalResults = count( $results );				// Get number of clean results
+do {
+	echo "INFO  : Grabbing data (Attempt $pageTryNumber of $retries)...\n";
 
-	foreach ( $results as $currentResult => $result ){
-	
-		$currentResult++;					// So it will show the right number in output, not index;
-		$result = 'http:' . trim( $result, '"' );		// Clean out quotes from URL and add HTTP to it
+	if ( $html = file_get_contents( $url ) ){
 
-		$filename = basename( $result );				// Get filename from URL.
+		echo "INFO  : Exploding data...\n";
+
+		preg_match_all( '/(")(\\/)(\\/)(i\\.4cdn\\.org).*?(")/is' , $html, $results );	// Just regex the files we're looking for.
+
+		$results = array_values( array_unique( $results[0] ) );		// Clean results from REGEX
+
+		$totalResults = count( $results );				// Get number of clean results
 		
-		echo "[$currentResult/$totalResults] $filename ";	// Simple echo to show user current position within thread
+		echo "INFO  : Found $totalResults media file(s)!\n";
 
-		if ( file_exists( $filename ) ) {
-
-			echo ".. Exists, ignoring!\n";
-			continue;
-
-		}
-
-		try{	
-			$fileData = file_get_contents( $result );				// Attempt to grab file from site's CDN
-		
-			if( $writeResult = file_put_contents( $filename, $fileData ) ){		// Attempt to write result to file
+		foreach ( $results as $currentResult => $result ){
 			
-				echo ".. OK! (". strlen($fileData)  . " bytes)\n";
+			$currentFileTries = 1;					// Number of tries we have on this file
 
-			} else {
+			$currentResult++;					// So it will show the right number in output, not index;
+			$result = 'http:' . trim( $result, '"' );		// Clean out quotes from URL and add HTTP to it
 
-				echo ".. ERROR!\n";
+			$filename = basename( $result );				// Get filename from URL.
+		
+			echo "[$currentResult/$totalResults] $filename ";	// Simple echo to show user current position within thread
 
-			}	
-		} catch (Exception $e) {
+			if ( file_exists( $filename ) ) {			// if like me you have a bad internet connection
+										// this if statement will be a blessing!
+				echo ".. Exists, ignoring!\n";
+				continue;
 
-			echo ".. ERROR : " . $e->getMessage() . "\n";
-		}
+			}
+
+			do {											// do-while loop, for retries. Still uses the user-changeable variable up top!
+				try{	
+					$fileData = file_get_contents( $result );				// Attempt to grab file from site's CDN
+		
+					if( $writeResult = file_put_contents( $filename, $fileData ) ){		// Attempt to write result to file
+			
+						echo ".. OK! (". formatBytes(strlen($fileData)) . ")\n";
+						break;
+
+					} else {
+
+						echo ".. ERROR!\n";
+
+					}	
+				} catch (Exception $e) {
+
+					echo ".. ERROR! \n";
+		
+					if ($currentFileTries != $retries ) echo "Retrying.. (Attempt $currentFileTries of $retries )\n";
+					
+					
+				}
+				$currentFileTries++;
+			} while ($currentFileTries <= $retries);
 	
+		}
+
+		echo "Done!\n";
+		die();
+
+	} else {
+
+		echo "ERROR : Thread possibly 404'd!\n";
+		
+
 	}
+	if ($pageTryNumber != $retries ) echo "INFO  : Retrying...\n"; else echo "FATAL : Could not connect after $retries tries! Aborting...\n";
+	$pageTryNumber++;
 
-	echo "Done!\n";
+} while ( $pageTryNumber <= $retries );
 
-} else {
+// Source for this function : http://stackoverflow.com/questions/2510434/format-bytes-to-kilobytes-megabytes-gigabytes
+// As stated... This is Quick-n-Dirty!
 
-	echo "ERROR : Thread possibly 404'd!\n";	
+function formatBytes($size, $precision = 2)
+{
+    $base = log($size) / log(1024);
+    $suffixes = array('', 'k', 'M', 'G', 'T');   
 
+    return round(pow(1024, $base - floor($base)), $precision) . $suffixes[floor($base)];
 }
